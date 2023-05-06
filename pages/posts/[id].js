7130ef7418eeaa016addef62a75d5bd5f57a632d
songling <songling@health-100.cn>
2023-05-06 09:30:10 +0800

    init

---
+++ pages/posts/[id].js
@@ -0,0 +1,46 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/date';
+import Head from 'next/head';
+import utilStyles from '../../styles/utils.module.css';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+/**
+ * getStaticPaths 和 getStaticProps 可以从任何数据源获取数据
+ * getAllPostIds 可以从外部API获取
+ * 注意fallback的使用：
+ *        1. fallback是false。则任何通过getStaticPaths未返回的路径都会return一个404页面
+ */
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
